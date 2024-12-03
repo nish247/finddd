9c99ead5c43dc3b3f12bccb89df3e1d1d2a8c7d7
nish247 <g2123046@fun.ac.jp>
2024-12-03 14:30:44 +0900

    Add function Upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import AudioRecorder from './component/recorder';
+import Upload from './component/Upload';
 
 function App() {
   return (
     <div className="App">
       <AudioRecorder />
+      <Upload />
     </div>
   );
 }
